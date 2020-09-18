31a266b906cbf1f21709fba7f5865718f629db93
Eduardo Quezada <eduardo602002@gmail.com>
2020-09-17 23:24:53 -0300

    Initial tests with Newtonsoft.Json.

--- moveParser/Form1.cs
+++ moveParser/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace moveParser
 {
@@ -15,6 +17,19 @@ namespace moveParser
         public Form1()
         {
             InitializeComponent();
+            dynamic pkmn_ps_data = JObject.Parse(File.ReadAllText("data/PS/learnsets.js"));
+
+            int cantidad = pkmn_ps_data.Count;
+            var a = pkmn_ps_data["bulbasaur"];
+            object b = a.learnset;
+
+            string[] propertyNames = b.GetType().GetProperties().Select(p => p.Name).ToArray();
+            foreach (var prop in propertyNames)
+            {
+                object propValue = b.GetType().GetProperty(prop).GetValue(b, null);
+            }
+
+            
         }
     }
 }
